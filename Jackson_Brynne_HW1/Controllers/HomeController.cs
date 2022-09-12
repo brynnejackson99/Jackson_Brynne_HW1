5e63db817879f7d3152d753191b5fb0d154d682b
brynnejackson99 <brynnejackson@utexas.edu>
2022-09-12 18:28:14 -0500

    Add project files.

---
+++ Jackson_Brynne_HW1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Jackson_Brynne_HW1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult ReturnResume()
+        {
+            string path = _environment.WebRootPath + "/files/BJResume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "BJResume.pdf");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        public IActionResult Travel()
+        {
+            return View();
+        }
+    }
+}
---
+++ Jackson_Brynne_HW1/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace Jackson_Brynne_HW1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This adds the MVC engine and Razor code
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //These lines allow you to see more detailed error messages
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+
+            //This line allows you to use static pages like style sheets and images
+            app.UseStaticFiles();
+
+            //This marks the position in the middleware pipeline where a routing decision
+            //is made for a URL.
+            app.UseRouting();
+
+            //This method maps the controllers and their actions to a patter for
+            //requests that's known as the default route. This route identifies
+            //the Home controller as the default controller and the Index() action
+            //method as the default action. The default route also identifies a
+            //third segment of the URL that's a parameter named id.
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
